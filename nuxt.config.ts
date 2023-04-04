292a9ea89b09ca4545ea1a41d844588734527963
AncuL001 <ancul001@gmail.com>
2023-04-04 12:16:47 +0700

    fix bootstrap interactive components not working

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    '@nuxtjs/google-fonts'
+    '@nuxtjs/google-fonts',
+  ],
+  plugins: [
+    '@/plugins/bootstrap.client.ts'
   ],
   googleFonts: {
     download: true,
