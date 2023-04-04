4a88928d8311afe2629f6557af27fa4e935da7e7
AncuL001 <ancul001@gmail.com>
2023-04-04 08:39:01 +0700

    create title component

---
+++ components/ImazineTitle.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <NuxtLink to="/" id="page-icon">
+                IMAZINE
+        </NuxtLink>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
