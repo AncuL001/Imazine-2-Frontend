a28e78f63962bd74bebfe05caaf3e18df91c828c
AncuL001 <ancul001@gmail.com>
2023-04-04 08:03:46 +0700

    fix google-fonts error by using prerelease ver

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  modules: [
+    '@nuxtjs/google-fonts'
+  ],
+  googleFonts: {
+    families: {
+      Lato: true,
+    }
+  },
+  css: [
+    'bootstrap/dist/css/bootstrap.min.css'
+  ],
 })
