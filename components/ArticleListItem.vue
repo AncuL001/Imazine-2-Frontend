54dfde7851347ac15e686c123ea93936ef30b31f
AncuL001 <ancul001@gmail.com>
2023-04-07 02:45:53 +0700

    current progress on styling

---
+++ components/ArticleListItem.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="px-4 py-0">
+        <NuxtLink class="article-list-item p-4 row align-items-center gx-5" to="#">
+                <div class="rectangle col-auto"></div>
+                <div class="col">
+                    <div class="row-auto">
+                        <span class="category-label-list text-primary">Category</span>
+                    </div>
+                    <h2 class="row-auto">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
+                    </h2>
+                </div>
+        </NuxtLink>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+* {
+    background-color: transparent;
+}
+
+.article-list-item {
+    border-radius: 24px;
+
+    .rectangle{
+        background-color: #D9D9D9;
+        width: 212px;
+        height: 142px;
+        border-radius: 16px;
+    }
+    * {
+        background-color: transparent;
+    }
+
+    col {
+        text-decoration: none;
+
+    }
+    &:hover {
+        background-color: #F5F5F5;
+    }
+}
+
+
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,28 @@
 <template>
-
+<div class="container py-4">
+  <div class="row align-items-start gx-4">
+    <div class="col">
+      <div class="content-container p-5">
+        <h1 class="pb-4">
+            Berita Terbaru
+        </h1>
+        <ArticleListItem />
+        <ArticleListItem />
+        <ArticleListItem />
+        <ArticleListItem />
+    </div>
+    </div>
+    <div class="col-2">
+      <div class="content-container p-4">
+        <h2>Kategori</h2>
+        <NuxtLink to="#" class="category-label-list">Category</NuxtLink>
+        <NuxtLink to="#" class="category-label-list">Category</NuxtLink>
+        <NuxtLink to="#" class="category-label-list">Category</NuxtLink>
+        <NuxtLink to="#" class="category-label-list">Category</NuxtLink>
+      </div>
+    </div>
+  </div>
+</div>
 </template>
 
 <script setup>
@@ -7,5 +30,11 @@
 </script>
 
 <style lang="scss" scoped>
-
+.content-container {
+    border-radius: 24px;
+    background-color: white;
+    * {
+        background-color: transparent;
+    }
+}
 </style>
