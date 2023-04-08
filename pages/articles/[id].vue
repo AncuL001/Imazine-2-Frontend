c8b3dc02dbcf3d4afa64e5cc98fe6f637b3bacfc
AncuL <ancul001@gmail.com>
2023-04-08 17:24:24 +0700

    make texts more readable

--- pages/articles/[id].vue
+++ pages/articles/[id].vue
@@ -13,7 +13,7 @@
                     Category
                 </NuxtLink>
                 <div class="vr mx-2"></div>
-                <span>
+                <span class="text-start">
                     3 hari yang lalu oleh 
                     <span class="fw-bold">Fauzan Azmi Dwicahyo</span>
                 </span>
@@ -21,13 +21,13 @@
         </div>
         <article>
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In nisl nisi scelerisque eu ultrices vitae auctor eu augue. Iaculis at erat pellentesque adipiscing commodo elit at. Turpis massa tincidunt dui ut. Sed arcu non odio euismod lacinia at. Fermentum iaculis eu non diam phasellus vestibulum lorem sed. A scelerisque purus semper eget. Risus pretium quam vulputate dignissim suspendisse in est. Auctor neque vitae tempus quam. Netus et malesuada fames ac turpis egestas maecenas pharetra. Eu consequat ac felis donec et odio pellentesque diam. Accumsan sit amet nulla facilisi morbi tempus iaculis. Metus dictum at tempor commodo ullamcorper a lacus vestibulum. Eget mi proin sed libero enim sed faucibus. Eget velit aliquet sagittis id consectetur. Elementum nibh tellus molestie nunc non blandit massa enim nec.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In nisl nisi scelerisque eu ultrices vitae auctor eu augue. Iaculis at erat pellentesque adipiscing commodo elit at. Turpis massa tincidunt dui ut. Sed arcu non odio euismod lacinia at. Fermentum iaculis eu non diam phasellus vestibulum lorem sed. A scelerisque purus semper eget. Risus pretium quam vulputate dignissim suspendisse in est.
             </p>
             <h2>
                 Lorem Ipsum
             </h2>
             <p>
-                Mauris a diam maecenas sed enim ut. Aliquet porttitor lacus luctus accumsan tortor posuere. Aliquam eleifend mi in nulla posuere sollicitudin aliquam ultrices. Pharetra sit amet aliquam id diam maecenas ultricies mi. Venenatis tellus in metus vulputate eu scelerisque. At varius vel pharetra vel turpis nunc eget lorem dolor. Lorem ipsum dolor sit amet consectetur adipiscing elit. Vulputate odio ut enim blandit volutpat maecenas volutpat blandit. Dolor sit amet consectetur adipiscing elit pellentesque habitant. Congue nisi vitae suscipit tellus mauris a diam maecenas sed. Congue nisi vitae suscipit tellus mauris a diam maecenas sed. Commodo ullamcorper a lacus vestibulum sed arcu. Nisi est sit amet facilisis magna etiam tempor orci eu.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In nisl nisi scelerisque eu ultrices vitae auctor eu augue. Iaculis at erat pellentesque adipiscing commodo elit at. Turpis massa tincidunt dui ut. Sed arcu non odio euismod lacinia at. Fermentum iaculis eu non diam phasellus vestibulum lorem sed. A scelerisque purus semper eget. Risus pretium quam vulputate dignissim suspendisse in est.
             </p>
             <p>
                 Commodo nulla facilisi nullam vehicula ipsum a arcu. Tellus id interdum velit laoreet id. Arcu felis bibendum ut tristique et egestas quis ipsum suspendisse. Eget dolor morbi non arcu risus quis varius quam. Vitae tempus quam pellentesque nec nam aliquam sem et tortor. Vestibulum sed arcu non odio. Vitae sapien pellentesque habitant morbi. Cursus vitae congue mauris rhoncus aenean vel. Sit amet risus nullam eget felis eget. Libero volutpat sed cras ornare arcu dui vivamus. Sit amet mauris commodo quis imperdiet massa tincidunt nunc.
@@ -50,6 +50,14 @@
         width: 100%;
         padding-bottom: 56.25%;
         border-radius: 16px;
+    }
+}
+article {
+    line-height: 1.75em;
+
+    h1, h2, h3, h4, h5, h6 {
+        margin-top: 1.5em;
+        margin-bottom: 0.75em;
     }
 }
 </style>
