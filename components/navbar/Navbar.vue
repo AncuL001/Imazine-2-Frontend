825d7ebc800c5e2c4c4a282132aba5c1dd22a0ed
AncuL <ancul001@gmail.com>
2023-04-08 16:52:49 +0700

    change container type

--- components/navbar/Navbar.vue
+++ components/navbar/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
     <nav class="navbar navbar-expand sticky-top bg-opacity-50 py-0">
-        <div class="container-xl">
+        <div class="container-lg">
             <NavbarImazineTitle />
             <NavbarNavs />
         </div>
