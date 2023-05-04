bf10101f3232551044f4ca5648a73ea2bf1d5c4d
AncuL001 <ancul001@gmail.com>
2023-05-04 23:49:14 +0700

    change v-for placement

--- components/HomePage/CategorySidebar.vue
+++ components/HomePage/CategorySidebar.vue
@@ -2,9 +2,9 @@
     <div class="category-card">
         <div class="content-container p-4">
             <h2>Kategori</h2>
-            <div v-for="category in categories" :key="category.id">
-                <NuxtLink :to="`/category/${category.id}`" class="category-label d-block">{{ category.name }}</NuxtLink>
-            </div>
+            <NuxtLink :to="`/category/${category.id}`" v-for="category in categories" :key="category.id" class="category-label d-block">
+                {{ category.name }}
+            </NuxtLink>
         </div>
     </div>
 </template>
