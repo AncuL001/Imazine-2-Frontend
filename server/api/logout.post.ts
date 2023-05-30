b4043f15d15ef00e4e230edf644caadfba80808a
AncuL001 <ancul001@gmail.com>
2023-05-30 18:54:36 +0700

    remove unused files

--- server/api/logout.post.ts
+++
@@ -1,5 +0,0 @@
-export default defineEventHandler(async (event) => {
-    event.context.session.isLoggedIn = false
-    event.context.session.user = null
-    return event.context.session.isLoggedIn;
-})
